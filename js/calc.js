a0d1e4589b0aeee75b53a942ebc5581d226d7819
stepmdon <stemcd@live.ie>
2013-11-15 23:51:32 +0000

    New Page added

--- js/calc.js
+++ js/calc.js
@@ -1,7 +1,9 @@
 // JavaScript Document
 function Calculator()
 {
-	 
+	this.rise = 0;
+	this.run = 0;
+	this.hypotenuse = 0; 
 }
 
 Calculator.prototype.text = function (val)
@@ -26,41 +28,146 @@ Calculator.prototype.equals = function ()
 	}
 }
 
-Calculator.prototype.pitch = function (val)
+Calculator.prototype.getPitch = function (val)
 {
-	var pitchValue = document.getElementById("dis").value
-	document.getElementById("pitchDis").value = val + ": " + pitchValue + '\xB0';
+	var pitchValue = calc.calcPitch();
+	document.getElementById("pitchDis").value = pitchValue ;
 }
 
-Calculator.prototype.rise = function (val)
+Calculator.prototype.getRise = function (val)
 {
 	var riseValue = document.getElementById("dis").value
-	document.getElementById("riseDis").value = val + ": " + riseValue;
+	document.getElementById("riseDis").value =  riseValue;
 }
 
-Calculator.prototype.run = function (val)
+Calculator.prototype.getRun = function (val)
 {
 	var runValue = document.getElementById("dis").value
-	document.getElementById("runDis").value = val + ": " + runValue;
+	document.getElementById("runDis").value = runValue;
 }
 
 Calculator.prototype.commonRafter = function (val)
 {
-	var commonRafterValue = document.getElementById("dis").value
-	document.getElementById("crDis").value = val + ": " + commonRafterValue;
+	
+		
+	rise = document.getElementById("riseDis").value;
+	run = document.getElementById("runDis").value;
+	hypotenuse = parseFloat(Math.sqrt(run * run + rise * rise)).toFixed(2);
+
+	commonRafterValue = document.getElementById("dis").value
+	document.getElementById("crDis").value =hypotenuse;
+	return hypotenuse
 }
 
-Calculator.prototype.jackRafter = function (val)
+Calculator.prototype.jackRafter = function (hypotenuse)
 {
-	var jackRafterValue = document.getElementById("dis").value
-	document.getElementById("jrDis").value = val + ": " + jackRafterValue;
+	rise = document.getElementById("riseDis").value;
+	run = document.getElementById("runDis").value;
+	hypotenuse = parseFloat(Math.sqrt(run * run + rise * rise)).toFixed(2);
+	var jackRafterValue = hypotenuse - 0.406;
+	document.getElementById("jrDis").value =  jackRafterValue;
 }
 
 Calculator.prototype.hipRafter = function (val)
 {
-	var hipRafterValue = document.getElementById("dis").value
+			
+	rise = document.getElementById("riseDis").value;
+	run = document.getElementById("runDis").value;
+	hipRafterValue = parseFloat(Math.sqrt(run * run * 2 + rise * rise)).toFixed(2);
 	document.getElementById("hipDis").value = val + ": " + hipRafterValue;
 }
 
-var calc = new Calculator();
+Calculator.prototype.calcPitch = function ()
+{		
+	rise = document.getElementById("riseDis").value;
+	run = document.getElementById("runDis").value;	
+	var tanX = rise / run;
+	var pitch = Math.atan(tanX);
+	var degrees = (pitch * (180 / Math.PI)).toFixed(2);
+	return degrees;
+}
 
+
+
+Calculator.prototype.open_NewWindow = function()
+{
+	//newWindow = window.open('','', 'left=100,top=100,width=300,height=300');
+	window.open('printPage.html','','left=10,top=10,width=200,height=200');
+	//newDocument = newWindow.document;
+	//document.getElementById("body").value = rise;
+	//newDocument.write(rise);
+	// calc.createTHead();
+	//var as = calc.calcPitch();
+	//document.getElementById("sss").value = as;
+}
+
+function close_Window()
+{
+	newWindow.close();
+}
+	
+/*createTHead = function()
+{
+	rise = document.getElementById("riseDis").value;
+    var x=document.getElementById("myTable");
+    if (!x.tHead)
+    {
+       var header=x.createTHead();
+       var row=header.insertRow(0);
+       var cell=row.insertCell(0);
+       cell.innerHTML= "tutuiiu";
+     
+  }
+}
+function deleteTHead()
+{
+document.getElementById("mainform").deleteTHead();
+}*/
+
+function newHTML() {
+
+HTMLstring  = '<HTML>\n';
+HTMLstring += '<HEAD>\n';
+HTMLstring += '<TITLE>Stephen doc</TITLE>\n';
+HTMLstring += '<link rel="stylesheet" type="text/css" href="../css/calc.css"/>'
+HTMLstring += '</HEAD>\n';
+HTMLstring += '<BODY>\n';
+HTMLstring += '<table id="myTable" border="1">'
+HTMLstring += '<tr>'
+HTMLstring += '<td>Pitch</td>'
+HTMLstring += '<td> '+mainform.pitchDisp.value+'</td>'
+HTMLstring += '</tr>'
+HTMLstring += '<tr>'
+HTMLstring += '<td>Rise</td>'
+HTMLstring += '<td>'+mainform.riseDisp.value+'</td>'
+HTMLstring += '</tr>'
+HTMLstring += '<tr>'
+HTMLstring += '<td>Run</td>'
+HTMLstring += '<td>'+mainform.runDisp.value+'</td>'
+HTMLstring += '</tr>'
+HTMLstring += '<tr>'
+HTMLstring += '<td>Common Rafter</td>'
+HTMLstring += '<td>'+mainform.crDisp.value+'</td>'
+HTMLstring += '</tr>'
+HTMLstring += '<tr>'
+HTMLstring += '<td>Hip/Valley</td>'
+HTMLstring += '<td>'+mainform.hipDisp.value+'</td>'
+HTMLstring += '</tr>'
+HTMLstring += '<tr>'
+HTMLstring += '<td>Jack Rafter</td>'
+HTMLstring += '<td>'+mainform.jrDisp.value+'</td>'
+HTMLstring += '</tr>'
+HTMLstring += '</table>'
+HTMLstring += '<br>'
+HTMLstring += '</BODY>\n';
+HTMLstring += '</HTML>';
+alert(HTMLstring);
+
+newwindow=window.open();
+newdocument=newwindow.document;
+newdocument.write(HTMLstring);
+
+}
+
+
+var calc = new Calculator();
